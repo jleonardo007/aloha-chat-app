53aee122cf674aa1cce5c4681f244128c323852d
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-12-08 01:20:23 -0400

    Main components added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
+import CreateUser from "./Components/CreateUser/CreateUser";
+import ChatPanel from "./Components/ChatPanel/ChatPanel";
+
 function App() {
-  return <div> Initial config</div>;
+   const [userDidCreated, setUserState] = useState(false);
+   const [user, setUser] = useState({});
+
+   const joinChat = (name, avatar) => {
+      setUser({
+         name,
+         avatar,
+      });
+
+      setUserState(!userDidCreated);
+   };
+
+   return userDidCreated ? (
+      <ChatPanel user={user} />
+   ) : (
+      <CreateUser joinChat={joinChat} />
+   );
 }
 
 export default App;
--- client/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("Check if tests works", () => {
-  render(<App />);
-  const element = screen.getByText(/initial config/i);
-  expect(element).toBeInTheDocument();
-});
