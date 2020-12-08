fda850207e2b32b2440ea808d23d9fe3abf55085
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-12-08 01:15:32 -0400

    CreateUser's test fixed.

--- client/src/Components/CreateUser/CreateUser.test.js
+++ client/src/Components/CreateUser/CreateUser.test.js
@@ -1,79 +1,67 @@
-import { screen, render } from "@testing-library/react";
+import { screen, render, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CreateUser from "./CreateUser";
 
-describe("CreateUser tests", () => {
-  test("Render user name section", () => {
-    render(<CreateUser />);
+test("User is created", async () => {
+   const joinChat = jest.fn();
+   render(<CreateUser joinChat={joinChat} />);
 
-    const welcomeTitle = screen.getByRole("heading", {
+   const welcomeTitle = screen.getByRole("heading", {
       name: /welcome/i,
       level: 1,
-    });
+   });
+   const nameLabel = screen.getByText(/your name:/i);
+   const nameInput = screen.getByRole("textbox");
+   const createNameButton = screen.getByRole("button", { name: /next/i });
 
-    const nameLabel = screen.getByLabelText(/your name:/i);
-    const nameInput = screen.getByRole("textbox");
-    const createNameButton = screen.getByRole("button");
+   expect(welcomeTitle).toBeInTheDocument();
+   expect(nameLabel).toBeInTheDocument();
+   expect(nameInput).toBeInTheDocument();
+   expect(nameInput).toHaveAttribute("maxlength", "30");
+   expect(nameInput).toBeRequired();
+   expect(createNameButton).toBeInTheDocument();
 
-    expect(welcomeTitle).toBeInTheDocument();
-    expect(nameLabel).toBeInTheDocument();
-    expect(nameInput).toBeInTheDocument();
-    expect(nameInput).toHaveAttribute("maxlength", 30);
-    expect(nameInput).toBeRequired();
-    expect(createNameButton).toBeInTheDocument();
-    expect(screen.queryByTestId("avatar-page")).not.toBeInTheDocument();
-  });
+   userEvent.type(nameInput, "");
+   userEvent.click(createNameButton);
+   expect(nameInput).toBeInvalid();
 
-  test("Create user name", () => {
-    render(<CreateUser />);
+   userEvent.type(nameInput, "John Doe");
+   userEvent.click(createNameButton);
+   expect(nameInput).toBeValid();
 
-    const nameInput = screen.getByRole("textbox");
-    const createNameButton = screen.getByRole("button");
+   await waitFor(() => {
+      expect(screen.queryByTestId("username-page")).not.toBeInTheDocument();
+   });
 
-    userEvent.type(nameInput, "");
-    userEvent.click(createNameButton);
-    expect(nameInput).toBeInvalid();
-
-    userEvent.type(nameInput, "very very very very long user name");
-    userEvent.click(createNameButton);
-    expect(nameInput).toBeInvalid();
-
-    userEvent.type(nameInput, "John Doe");
-    userEvent.click(createNameButton);
-    expect(nameInput).toBeValid();
-  });
-
-  test("Render avatar section", () => {
-    render(<CreateUser />);
-
-    const avatarTitle = screen.getByRole("heading", {
-      name: /pick an avatar/i,
+   const avatarTitle = await screen.findByRole("heading", {
+      name: /pick an avatar!/i,
       level: 1,
-    });
-    const avatarPreview = screen.getByAltText(/avatar preview/i);
-    const avatarsCollection = screen.getAllByAltText(/avatar/i);
+   });
+   const avatarPreview = await screen.findByAltText(/avatar preview/i);
+   const avatarsCollection = await screen.findAllByAltText(/user avatar/i);
+   const randomAvatar = Math.floor(Math.random() * 9);
 
-    expect(screen.queryByTestId("name-page")).not.toBeInTheDocument();
+   expect(avatarTitle).toBeInTheDocument();
+   expect(avatarPreview).toBeInTheDocument();
+   expect(avatarPreview).toHaveAttribute("loading", "lazy");
+   expect(avatarsCollection[randomAvatar]).toBeInTheDocument();
+   expect(avatarsCollection[randomAvatar]).toHaveAttribute("loading", "lazy");
 
-    expect(avatarTitle).toBeInTheDocument();
-    expect(avatarPreview).toBeInTheDocument();
-    expect(avatarPreview).toHaveAttribute("loading", "lazy");
-    expect(avatarsCollection).toBeInTheDocument();
-    expect(avatarsCollection).toHaveAttribute("loading", "lazy");
-  });
+   expect(
+      screen.queryByRole("button", {
+         name: /join!/i,
+      })
+   ).not.toBeInTheDocument();
 
-  test("Select an avatar", async () => {
-    render(<CreateUser />);
+   userEvent.click(avatarsCollection[randomAvatar]);
 
-    const avatarPreview = screen.getByAltText(/avatar preview/i);
-    const avatarsCollection = screen.getAllByAltText(/avatar/i);
+   expect(avatarPreview.src).toEqual(avatarsCollection[randomAvatar].src);
 
-    userEvent.click(avatarsCollection[0]);
-    expect(avatarPreview.src).toEqual(avatarsCollection[0].src);
-
-    const joinChatBtn = await screen.findByRole("button", {
+   const joinChatBtn = await screen.findByRole("button", {
       name: /join!/i,
-    });
-    expect(joinChatBtn).toBeInTheDocument();
-  });
+   });
+
+   expect(joinChatBtn).toBeInTheDocument();
+   userEvent.click(joinChatBtn);
+   expect(joinChat).toHaveBeenCalledTimes(1);
 });
