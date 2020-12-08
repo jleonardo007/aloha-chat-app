08cef99ec529f1ae1103289db4657ca607ecde87
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-12-08 01:16:10 -0400

    CreateUser component terminated.

--- client/src/Components/CreateUser/CreateUser.js
+++ client/src/Components/CreateUser/CreateUser.js
@@ -1,52 +1,122 @@
+import { useState } from "react";
+
 import "./CreateUser.css";
-import avatar from "../../default-avatars/astronauta.png";
+import astronauta from "../../default-avatars/astronauta.png";
+import hacker from "../../default-avatars/hacker.png";
+import niña1 from "../../default-avatars/nina-1.png";
+import niña2 from "../../default-avatars/nina-2.png";
+import niña3 from "../../default-avatars/nina-3.png";
+import niña4 from "../../default-avatars/nina-4.png";
+import ninja from "../../default-avatars/ninja.png";
+import rey from "../../default-avatars/rey.png";
+import noAvatar from "../../default-avatars/no-avatar.png";
 
-function CreateUserName() {
-  return (
-    <section className="welcome-page">
-      <div className="welcome__container">
-        <div className="welcome__title"></div>
-        <div className="create-name-container"></div>
-      </div>
-    </section>
-  );
+const avatarsCollection = [
+   astronauta,
+   hacker,
+   niña1,
+   niña2,
+   niña3,
+   niña4,
+   ninja,
+   rey,
+];
+
+function CreateUserName({ setName }) {
+   const [inputValue, setValue] = useState("");
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      setName(inputValue);
+   };
+
+   return (
+      <section className='welcome-page' data-testid='username-page'>
+         <div className='welcome__container'>
+            <div className='welcome__title'>
+               <h1>Welcome!</h1>
+            </div>
+            <div className='create-name-container'>
+               <form className='form' onSubmit={(e) => handleSubmit(e)}>
+                  <div className='form-label'>
+                     <label htmlFor='user-name'>Your name:</label>
+                  </div>
+                  <div className='form-input'>
+                     <input
+                        type='text'
+                        required
+                        maxLength={30}
+                        id='user-name'
+                        value={inputValue}
+                        onChange={(e) => setValue(e.target.value)}
+                     />
+                  </div>
+                  <div className='form-btn'>
+                     <button type='submit'>Next</button>
+                  </div>
+               </form>
+            </div>
+         </div>
+      </section>
+   );
 }
 
-function CreateUserAvatar() {
-  return (
-    <section className="avatar-page">
-      <div className="avatar__title">
-        <h1>texto</h1>
-      </div>
-      <div className="avatar__container">
-        <div className="avatar__preview">
-          <img src={avatar} alt="User avatar" className="avatar-image" />
-          <div className="join-btn-container"></div>
-        </div>
-        <div className="default-avatars">
-          <div className="collection">
-            <img src={avatar} alt="User avatar" className="avatar-image" />
-            <div className="avatar-image"></div>
-            <div className="avatar-image"></div>
-            <div className="avatar-image"></div>
-            <div className="avatar-image"></div>
-            <div className="avatar-image"></div>
-            <div className="avatar-image"></div>
-            <div className="avatar-image"></div>
-            <div className="avatar-image"></div>
-            <div className="avatar-image"></div>
-            <div className="avatar-image"></div>
-            <div className="avatar-image"></div>
-          </div>
-          <div className="upload-avatar"></div>
-        </div>
-      </div>
-    </section>
-  );
+function CreateUserAvatar({ avatar, setAvatar, join }) {
+   return (
+      <section className='avatar-page' data-testid='avatar-page'>
+         <div className='avatar__title'>
+            <h1>Pick an avatar!</h1>
+         </div>
+         <div className='avatar__container'>
+            <div className='avatar__preview'>
+               <img
+                  src={avatar ? avatar : noAvatar}
+                  className='avatar-image'
+                  alt='Avatar preview'
+                  loading='lazy'
+               />
+               <div className='join-btn-container'>
+                  {avatar ? (
+                     <button onClick={() => join()}>Join!</button>
+                  ) : null}
+               </div>
+            </div>
+            <div className='default-avatars'>
+               <div className='collection'>
+                  {avatarsCollection.map((avatarSrc, index) => {
+                     return (
+                        <img
+                           key={index}
+                           src={avatarSrc}
+                           alt='User avatar'
+                           className='avatar-image'
+                           loading='lazy'
+                           onClick={(e) => {
+                              setAvatar(e.target.src);
+                           }}
+                        />
+                     );
+                  })}
+               </div>
+            </div>
+         </div>
+      </section>
+   );
 }
 
-function CreateUser() {
-  return !true ? <CreateUserName /> : <CreateUserAvatar />;
+function CreateUser({ joinChat }) {
+   const [userName, setName] = useState("");
+   const [userAvatar, setAvatar] = useState("");
+
+   const join = () => {
+      joinChat(userName, userAvatar);
+   };
+
+   return !userName ? (
+      <CreateUserName setName={setName} />
+   ) : (
+      <CreateUserAvatar avatar={userAvatar} setAvatar={setAvatar} join={join} />
+   );
 }
 
 export default CreateUser;
