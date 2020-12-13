914b1594a31516573a79f1978bee46bcd0df33bb
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-12-13 02:28:31 -0400

    Socket client added.

---
+++ client/src/socket-client.js
@@ -0,0 +1,6 @@
+import { io } from "socket.io-client";
+const url = process.env.NODE_ENV === "test" ? "" : "http://localhost:4000";
+
+const socket = io(url, { path: "/" });
+
+export default socket;
