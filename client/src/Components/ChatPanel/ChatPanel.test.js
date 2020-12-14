6e70951598a5326dc54ab2f30f91750126110bc6
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-12-14 00:04:23 -0400

    Rewrited ChatPanel test's.

--- client/src/Components/ChatPanel/ChatPanel.test.js
+++ client/src/Components/ChatPanel/ChatPanel.test.js
@@ -1,110 +1,103 @@
-import { screen, render, waitFor } from "@testing-library/react";
+import { screen, render, waitFor, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import MockedSocket from "socket.io-mock";
-
-import CreateUser from "../../Components/CreateUser/CreateUser";
+import testSocket from "../../test_utils/testSocket";
 import ChatPanel from "./ChatPanel";
+
 import userAvatar1 from "../../default-avatars/astronauta.png";
 import userAvatar2 from "../../default-avatars/nina-2.png";
 import userAvatar3 from "../../default-avatars/rey.png";
 
-describe("Chat panel test", () => {
-  test("Render user info and chat's initial state", () => {
-    const createUser = render(<CreateUser />).queryByRole("region");
-    expect(createUser).not.toBeInTheDocument();
+const mockedUser = {
+   name: "Black Mamba",
+   avatar: userAvatar1,
+};
 
-    render(<ChatPanel />);
+const mockActiveUsers = [
+   {
+      name: "John Doe",
+      avatar: userAvatar1,
+   },
+   {
+      name: "Jane Doe",
+      avatar: userAvatar2,
+   },
+   {
+      name: "King T'Chala",
+      avatar: userAvatar3,
+   },
+];
 
-    const userInfo = screen.getByTestId("user-info");
+describe("Chat panel tests", () => {
+   beforeEach(() => {
+      render(<ChatPanel user={mockedUser} />);
+   });
 
-    const noChatImg = screen.getByAltText(/start talk/i);
-    const noChatTitle = screen.getByText(/talk to somebody/i);
+   test("User info are showed", () => {
+      const userInfo = screen.getByTestId("user-info");
 
-    expect(userInfo).toContainElement(screen.getByRole("img"));
-    expect(userInfo).toContainElement(screen.getByRole("list"));
-    expect(userInfo).toContainElement(screen.getByText(/jonh doe/i));
-    expect(noChatImg).toBeInTheDocument();
-    expect(noChatTitle).toBeInTheDocument();
-    expect(screen.getByRole("img")).toHaveAttribute("loading", "lazy");
-  });
+      expect(userInfo).toContainElement(screen.getByAltText(/User avatar/i));
+      expect(userInfo).toContainElement(screen.getByText("Black Mamba"));
+   });
 
-  test("Render active users empty list", () => {
-    render(<ChatPanel />);
+   test("Info are showed when user not chating", () => {
+      const noChatImg = screen.getByAltText(/start talk/i);
+      const noChatTitle = screen.getByText(/talk to somebody/i);
 
-    const socket = new MockedSocket();
-    const noUsersImg = screen.getByAltText(/no users/i);
-    const noUsersTitle = screen.getByText(/no active users/i);
-    const activeUsers = screen.queryAllByTestId(/active user/i);
-    const mockActiveUsers = [];
+      expect(noChatImg).toBeInTheDocument();
+      expect(noChatTitle).toBeInTheDocument();
+   });
 
-    socket.on("get:active-users", (activeUsers) => {
-      expect(activeUsers).toHaveLength(0);
-    });
+   test("Show list when there is not active users", () => {
+      const noUsersImg = screen.getByAltText(/no users/i);
+      const noUsersTitle = screen.getByText(/no active users/i);
+      const activeUsers = screen.queryAllByTestId(/active user/i);
 
-    socket.socketClient.emit("get:active-users", mockActiveUsers);
+      activeUsers.forEach((activeUser) => {
+         expect(activeUser).not.toBeInTheDocument();
+      });
+      expect(noUsersImg).toBeInTheDocument();
+      expect(noUsersTitle).toBeInTheDocument();
+   });
 
-    expect(activeUsers).not.toBeInTheDocument();
-    expect(noUsersImg).toBeInTheDocument();
-    expect(noUsersTitle).toBeInTheDocument();
-    expect(screen.getByRole("img")).toHaveAttribute("loading", "lazy");
-  });
+   test("Show list when there is active users", async () => {
+      act(() => {
+         testSocket.emit("test:active-users", mockActiveUsers);
+      });
 
-  test("Render active users list", () => {
-    render(<ChatPanel />);
+      const activeUsers = await screen.findAllByTestId("active-user");
 
-    const socket = new MockedSocket();
-    const mockActiveUsers = [
-      {
-        name: "John Doe",
-        avatarPath: userAvatar1,
-      },
-      {
-        name: "Jane Doe",
-        avatarPath: userAvatar2,
-      },
-      {
-        name: "King T'chala",
-        avatarPath: userAvatar3,
-      },
-    ];
+      expect(activeUsers).toHaveLength(mockActiveUsers.length);
+      activeUsers.forEach((activeUser, index) => {
+         expect(activeUser).toBeInTheDocument();
+         expect(activeUser).not.toBeEmptyDOMElement();
+         expect(activeUser).toHaveTextContent(mockActiveUsers[index].name);
+         expect(activeUser).toContainElement(
+            screen.getByAltText(mockActiveUsers[index].name)
+         );
+      });
+   });
 
-    socket.on("get:active-users", (activeUsers) => {
-      expect(activeUsers).toHaveLength(3);
-      expect(activeUsers).toEqual(mockActiveUsers);
-    });
+   test("Start talk with another users", async () => {
+      act(() => {
+         testSocket.emit("test:active-users", mockActiveUsers);
+      });
 
-    socket.socketClient.emit("get:active-users", mockActiveUsers);
+      const activeUsers = await screen.findAllByTestId("active-user");
+      const randomActiveUser = Math.floor(Math.random() * 3);
 
-    const activeUsers = screen.getAllByTestId("active-user");
+      await waitFor(() => {
+         userEvent.click(activeUsers[randomActiveUser]);
+      });
 
-    expect(activeUsers).toHaveLength(mockActiveUsers.length);
-    expect(activeUsers).toBeInTheDocument();
-    expect(activeUsers).not.toBeEmptyDOMElement();
-    expect(activeUsers).toContainElement(screen.getByRole("img"));
-    expect(activeUsers).toHaveTextContent();
-    expect(screen.getByRole("img")).toHaveAttribute("loading", "lazy");
-  });
-
-  test("Render chat", async () => {
-    render(<ChatPanel />);
-
-    const activeUsers = screen.getAllByTestId("active-user");
-
-    userEvent.click(activeUsers[1]);
-
-    await waitFor(() => {
       expect(screen.queryByAltText(/start talk/i)).not.toBeInTheDocument();
       expect(screen.queryByText(/talk to somebody/i)).not.toBeInTheDocument();
       expect(screen.queryByTestId("chat")).toBeInTheDocument();
-      expect(screen.queryByTestId("chat")).not.toBeEmptyDOMElement();
-    });
 
-    const receptor = await screen.findByTestId("receptor");
+      const receptor = await screen.findByTestId("receptor");
 
-    expect(receptor).toBeInTheDocument();
-    expect(receptor).toContainElement(screen.getByRole("img"));
-    expect(receptor).toContainElement(screen.getByRole("list"));
-    expect(receptor).toContainElement(screen.getByText(/jane doe/i));
-    expect(screen.getByRole("img")).toHaveAttribute("loading", "lazy");
-  });
+      expect(receptor).toBeInTheDocument();
+      expect(receptor).toHaveTextContent(
+         mockActiveUsers[randomActiveUser].name
+      );
+   });
 });
