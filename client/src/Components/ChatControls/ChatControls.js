34c601cdb60a5f1acf67f2b27d3f576cd7236d0b
Leonardo <j.leonardo.bravo@gmail.com>
2021-01-22 00:28:29 -0400

    Dialog to cancel voice note added.

--- client/src/Components/ChatControls/ChatControls.js
+++ client/src/Components/ChatControls/ChatControls.js
@@ -1,6 +1,7 @@
 import { GrEmoji } from "react-icons/gr";
 import { BsFillMicFill } from "react-icons/bs";
 import { BiSend } from "react-icons/bi";
+import { FaAngleLeft } from "react-icons/fa";
 import Picker from "emoji-picker-react";
 
 import "./ChatControls.css";
@@ -20,9 +21,22 @@ function ChatControls() {
 				{true ? <GrEmoji /> : <BsFillMicFill className="animated-mic" />}
 			</button>
 			<div className="messages-input-container">
-				<span className="message-input" role="textbox" contentEditable></span>
+				{true ? (
+					<span className="message-input" role="textbox" contentEditable></span>
+				) : (
+					<div className="cancel-dialog">
+						<p>
+							<span>
+								<FaAngleLeft />
+							</span>{" "}
+							Swipe to cancel
+						</p>
+					</div>
+				)}
 			</div>
-			<button className="voice-note-btn">{true ? <BsFillMicFill /> : <BiSend />}</button>
+			<button className="voice-note-btn" draggable>
+				{true ? <BsFillMicFill /> : <BiSend />}
+			</button>
 			{!true ? (
 				<Picker pickerStyle={emojiPickerStyle} disableAutoFocus={true} disableSearchBar={true} />
 			) : null}
