7b286f724c414e1dba960d48f3ee02cb6b860356
Leonardo <j.leonardo.bravo@gmail.com>
2021-01-21 21:57:42 -0400

    Removed static message array and relocated in test_utils folder.

--- client/src/Components/Messages/Messages.js
+++ client/src/Components/Messages/Messages.js
@@ -1,9 +1,8 @@
 import { BiCheckDouble } from "react-icons/bi";
 import { BsPlayFill, BsPauseFill } from "react-icons/bs";
-import "./Messages.css";
+import mockMessages from "../../test_utils/mockMessages";
 
-import audioSample from "../../test_utils/audio_sample.mp3";
-import userAvatar from "../../default-avatars/rey.png";
+import "./Messages.css";
 
 function TextMessages({ status, content }) {
 	return (
@@ -46,81 +45,9 @@ function VoiceNote({ user, message }) {
 }
 
 function Messages(/*{ messages }*/) {
-	const messages = [
-		{
-			type: "text",
-			status: "send",
-			content: "Hello world!!",
-			user: {
-				name: "",
-				avatar: "",
-			},
-		},
-		{
-			type: "text",
-			status: "received",
-			content: "whats uuuuup!",
-			user: {
-				name: "",
-				avatar: "",
-			},
-		},
-		{
-			type: "text",
-			status: "received",
-			content: "btw i use arch linux",
-			user: {
-				name: "",
-				avatar: "",
-			},
-		},
-		{
-			type: "text",
-			status: "send",
-			content: "me tooo!",
-			user: {
-				name: "",
-				avatar: "",
-			},
-		},
-		{
-			type: "text",
-			status: "received",
-			content: `
-				We use command-line utilities every day, whether it be git, grep, awk, npm, or any other terminal app. CLIs are super useful and usually the fastest way to get something done. Do you have something in your specific workflow that you have to do over and over again? Chances are that can be automated with a CLI.
-
-				We are going to use Node.js to make our CLI if it wasn’t clear from the title itself. Why? Because the Node.js ecosystem has thousands of extremely useful packages that we can utilize to achieve what we are trying to do. Whatever it may be that you are trying to do, it is highly probable that there exists a package for it on npm, also node has built-in libraries to do a lot of things like handling files, launching other applications, asserting tests, etc. Apart from that CLIs built in Node.js
-				are highly portable, meaning they are easy to install on different OSs.
-			
-				For the purpose of this tutorial, we’ll be building a simple CLI to translate between languages. We’ll accept string type arguments, parse them into a sentence, shoot them off to a translation API which will fetch us the translations, and then display the result. The complete code for this can be found on the Github repository. Let’s dive right into it!`,
-			user: {
-				name: "",
-				avatar: "",
-			},
-		},
-		{
-			type: "voice-note",
-			status: "send",
-			content: audioSample,
-			user: {
-				name: "Jonh Doe",
-				avatar: userAvatar,
-			},
-		},
-		{
-			type: "voice-note",
-			status: "received",
-			content: audioSample,
-			user: {
-				name: "Jonh Doe",
-				avatar: userAvatar,
-			},
-		},
-	];
-
 	return (
 		<ul className="messages">
-			{messages.map((message, index) => {
+			{mockMessages.map((message, index) => {
 				return (
 					<li className="message" key={index}>
 						{!true ? <input type="checkbox" className="select-box" /> : null}
