968f34815e1f0b5e6e0e87377b2b2fb9557d487e
Leonardo <j.leonardo.bravo@gmail.com>
2021-03-06 00:43:28 -0400

    Chat messages object added.

--- client/src/Components/ChatPanel/ChatPanel.js
+++ client/src/Components/ChatPanel/ChatPanel.js
@@ -10,20 +10,100 @@ import talk from "../../chat-icons/charla.png";
 import "./ChatPanel.css";
 
 function ChatPanel(props) {
-	const [background, setBackground] = useState({
-		color: "#013220",
-		toggleColor: "",
+	const [chatMessagesObject, setChatMessagesObject] = useState({
+		sentMessage: null,
+		receivedMessage: null,
+		undeliveredMessages: [],
 	});
 
 	useEffect(() => {
 		if (!props.user.id) socketClient.emit("new-user", props.user);
+
 		socketClient.on("user-connected", (user) => {
 			props.dispatch({
 				type: "USER_CONNECTED",
 				user,
 			});
 		});
-	}, [props]);
+
+		return () => {
+			socketClient.off("user-connected");
+		};
+	});
+
+	useEffect(() => {
+		socketClient.on("new-message", (message) => {
+			if (props.friend)
+				setChatMessagesObject((prevState) => {
+					if (message.from.id === props.friend.id)
+						return {
+							...prevState,
+							receivedMessage: { ...message, status: "received" },
+						};
+					else
+						return {
+							...prevState,
+							undeliveredMessages: [
+								...prevState.undeliveredMessages,
+								{ ...message, status: "received" },
+							],
+						};
+				});
+			else
+				setChatMessagesObject((prevState) => {
+					return {
+						...prevState,
+						undeliveredMessages: [
+							...prevState.undeliveredMessages,
+							{ ...message, status: "received" },
+						],
+					};
+				});
+		});
+
+		if (props.friend)
+			setChatMessagesObject((prevState) => {
+				if (prevState.undeliveredMessages.some((message) => message.from.id === props.friend.id))
+					return {
+						...prevState,
+						receivedMessage: prevState.undeliveredMessages.filter(
+							(message) => message.from.id === props.friend.id
+						),
+						undeliveredMessages: prevState.undeliveredMessages.filter(
+							(message) => message.from.id !== props.friend.id
+						),
+					};
+				else return prevState;
+			});
+
+		return () => {
+			setChatMessagesObject((prevState) => {
+				if (!prevState.sentMessage && !prevState.receivedMessage) return prevState;
+				else
+					return {
+						...prevState,
+						sentMessage: null,
+						receivedMessage: null,
+					};
+			});
+
+			socketClient.off("new-message");
+		};
+	}, [props.friend]);
+
+	useEffect(() => {
+		if (
+			props.chatConfigObject.shouldEmptyChat ||
+			props.chatConfigObject.shouldDeleteSelectedMessages
+		)
+			setChatMessagesObject((prevState) => {
+				return {
+					...prevState,
+					sentMessage: null,
+					receivedMessage: null,
+				};
+			});
+	}, [props.chatConfigObject]);
 
 	return (
 		<section className="chat-panel">
@@ -36,12 +116,12 @@ function ChatPanel(props) {
 					) : props.settingOption === "background-settings" ? (
 						<Backgrounds
 							dispatch={props.dispatch}
-							background={background}
-							setBackground={setBackground}
+							chatConfigObject={props.chatConfigObject}
+							setChatConfigObject={props.setChatConfigObject}
 						/>
-					) : props.settingOption === "theme-settings" ? (
-						<Theme dispatch={props.dispatch} />
-					) : null}
+					) : (
+						props.settingOption === "theme-settings" && <Theme dispatch={props.dispatch} />
+					)}
 				</div>
 			)}
 
@@ -56,10 +136,23 @@ function ChatPanel(props) {
 				<div className="chat" data-testid="chat">
 					<div className="friend-container">{props.friendComponent}</div>
 					<div className="messages-container">
-						<Messages background={background} />
+						<Messages
+							userId={props.user.id}
+							friendId={props.friend.id}
+							sentMessage={chatMessagesObject.sentMessage}
+							receivedMessage={chatMessagesObject.receivedMessage}
+							chatConfigObject={props.chatConfigObject}
+							setChatConfigObject={props.setChatConfigObject}
+						/>
 					</div>
 					<div className="chat-controls-container">
-						<ChatControls />
+						<ChatControls
+							friend={props.friend}
+							user={props.user}
+							setSentMessage={setChatMessagesObject}
+							chatConfigObject={props.chatConfigObject}
+							setChatConfigObject={props.setChatConfigObject}
+						/>
 					</div>
 				</div>
 			)}
