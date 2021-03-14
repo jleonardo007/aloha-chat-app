3ba063d69c4b43d6527481d60c7e5ed5ec6c9531
Leonardo <j.leonardo.bravo@gmail.com>
2021-03-14 19:07:04 -0400

    Dispatch added in Theme.

--- client/src/Components/Theme/Theme.js
+++ client/src/Components/Theme/Theme.js
@@ -1,18 +1,20 @@
 import "./Theme.css";
 
-function Theme() {
+function Theme({ dispatch }) {
 	return (
-		<div className="theme-container">
+		<div className="theme-container" data-testid="theme">
 			<div className="light-theme">
 				<label htmlFor="light">Light Theme</label>
-				<input type="radio" name="theme" id="light" checked />
+				<input type="radio" name="theme" id="light" defaultChecked />
 			</div>
 			<div className="dark-theme">
 				<label htmlFor="dark">Dark Theme</label>
 				<input type="radio" name="theme" id="dark" />
 			</div>
 			<div className="back-btn-container">
-				<button className="back-btn">Back</button>
+				<button className="back-btn" onClick={() => dispatch({ type: "RENDER_CHAT_INFO" })}>
+					Back
+				</button>
 			</div>
 		</div>
 	);
